2bc77699eb6906a6795fe1bfc7357978f45b2d1a
hostship <khaled@uvic.ca>
2016-02-19 20:15:05 -0800

    initial code

--- initial.js
+++ initial.js
@@ -1,1 +1,33 @@
-initial commit
+function yourScript() {
+	
+  
+  function basicmotion(){
+      if ( self.head_y != self.food_y){
+          if ( self.head_y > self.food_y){
+            
+            return UP;
+          }
+           if ( self.head_y < self.food_y){
+            return DOWN;
+          }
+      }
+      
+      
+      
+       if ( self.head_x > self.food_x){
+        return LEFT;
+      }
+       if ( self.head_x < self.food_x){
+        return RIGHT;
+      }
+      
+      
+      if ( self.head_x == self.head_x){
+        return UP;
+      }
+  
+  }
+  
+  
+}
+
